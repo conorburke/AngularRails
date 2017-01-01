43f69a4c7d86d531475179897c58523dcd1c247c
conorburke <cjburke89@gmail.com>
2017-01-01 15:39:11 -0800

    set up proposal class and test functionality

---
+++ app/proposal/proposal-list.component.js
@@ -0,0 +1,30 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var proposal_1 = require("./proposal");
+var ProposalListComponent = (function () {
+    function ProposalListComponent() {
+        this.proposalOne = new proposal_1.Proposal(1, 'Kevin Burke Experiment', 'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15, 'conor@seespotgo.com');
+        this.proposalTwo = new proposal_1.Proposal(2, 'Haulr', 'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15, 'conor@seespotgo.com');
+        this.proposalThree = new proposal_1.Proposal(3, 'Haulr Experiment', 'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15, 'conor@seespotgo.com');
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
+    }
+    return ProposalListComponent;
+}());
+ProposalListComponent = __decorate([
+    core_1.Component({
+        moduleId: module.id,
+        selector: 'proposal-list',
+        templateUrl: 'proposal-list.component.html'
+    })
+], ProposalListComponent);
+exports.ProposalListComponent = ProposalListComponent;
---
+++ app/proposal/proposal-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Proposal } from './proposal';
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-list',
+  templateUrl: 'proposal-list.component.html'
+})
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(1, 'Kevin Burke Experiment',
+    'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15,
+    'conor@seespotgo.com'),
+  proposalTwo: Proposal = new Proposal(2, 'Haulr',
+    'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15,
+    'conor@seespotgo.com'),
+  proposalThree: Proposal = new Proposal(3, 'Haulr Experiment',
+    'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15,
+    'conor@seespotgo.com'),
+
+  proposals: Proposal[] = [
+    this.proposalOne, 
+    this.proposalTwo, 
+    this.proposalThree
+  ]
+}
