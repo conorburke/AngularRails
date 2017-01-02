afc7b2e9b9bdc8727cf47d62841c7dc2b111c8e1
conorburke <cjburke89@gmail.com>
2017-01-02 14:50:22 -0800

    set up preferred styling for fe components

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -18,21 +18,21 @@ var DocumentsComponent = (function () {
                 description: "an angular experiment",
                 file_url: 'www.google.com',
                 updated_at: '1/1/2017',
-                image_url: 'www.google.com'
+                image_url: 'https://c2.staticflickr.com/6/5490/14525128103_08a505b99c_b.jpg'
             },
             {
                 title: "My second doc",
                 description: "an angular experiment",
                 file_url: 'www.google.com',
                 updated_at: '1/1/2017',
-                image_url: 'www.google.com'
+                image_url: 'https://c2.staticflickr.com/4/3925/15354675906_4a5a05d088_b.jpg'
             },
             {
                 title: "My third doc",
                 description: "an angular experiment",
                 file_url: 'www.google.com',
                 updated_at: '1/1/2017',
-                image_url: 'www.google.com'
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/STS-135_final_flyaround_of_ISS_1.jpg/500px-STS-135_final_flyaround_of_ISS_1.jpg'
             }
         ];
     }
@@ -42,7 +42,8 @@ DocumentsComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: 'documents',
-        templateUrl: 'documents.component.html'
+        templateUrl: 'documents.component.html',
+        styleUrls: ['documents.component.css']
     }),
     __metadata("design:paramtypes", [])
 ], DocumentsComponent);
--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -4,7 +4,8 @@ import { Document } from './document';
 @Component({ 
   moduleId: module.id,
   selector: 'documents',
-  templateUrl: 'documents.component.html'
+  templateUrl: 'documents.component.html',
+  styleUrls: ['documents.component.css']
 })
 
 export class DocumentsComponent {
@@ -16,21 +17,21 @@ export class DocumentsComponent {
       description: "an angular experiment",
       file_url: 'www.google.com',
       updated_at: '1/1/2017',
-      image_url: 'www.google.com'
+      image_url: 'https://c2.staticflickr.com/6/5490/14525128103_08a505b99c_b.jpg'
     },
     {
       title: "My second doc",
       description: "an angular experiment",
       file_url: 'www.google.com',
       updated_at: '1/1/2017',
-      image_url: 'www.google.com'
+      image_url: 'https://c2.staticflickr.com/4/3925/15354675906_4a5a05d088_b.jpg'
     },
     {
       title: "My third doc",
       description: "an angular experiment",
       file_url: 'www.google.com',
       updated_at: '1/1/2017',
-      image_url: 'www.google.com'
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/STS-135_final_flyaround_of_ISS_1.jpg/500px-STS-135_final_flyaround_of_ISS_1.jpg'
     }
 
   ]
