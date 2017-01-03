7e3ed909c67d028fef5ca1f7533d3e4b029fc89d
conorburke <cjburke89@gmail.com>
2017-01-02 17:49:58 -0800

    set up initial show routes

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,26 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
-  styleUrls: ['proposal-list.component.css']
+  styleUrls: ['proposal-list.component.css'],
+  providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(1, 'Kevin Burke Experiment',
-    'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15,
-    'conor@seespotgo.com');
-  proposalTwo: Proposal = new Proposal(2, 'Haulr',
-    'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15,
-    'conor@seespotgo.com');
-  proposalThree: Proposal = new Proposal(3, 'Haulr Experiment',
-    'http://portfolio.conor.com', 'Ruby on Rails', 150, 120, 15,
-    'conor@seespotgo.com');
+export class ProposalListComponent implements OnInit {
+  proposals: Proposal[];
+  errorMessage: string;
+  mode = "Observable";
 
-  proposals: Proposal[] = [
-    this.proposalOne, 
-    this.proposalTwo, 
-    this.proposalThree
-  ]
+  constructor(
+    private proposalService: ProposalService 
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
+
+  getProposals(){
+    this.proposalService.getProposals()
+        .subscribe(
+           proposals => this.proposals = proposals,
+           error => this.errorMessage = <any>error
+        );
+  }
 }
